1dbc43d21acfcf244e0fa526a7dc713f36a03d56
Joshua Sandlin <joshua.sandlin@gmail.com>
2015-09-15 17:32:09 -0400

    triangles are fun

--- triangle.rb
+++ triangle.rb
@@ -14,7 +14,42 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  # push them and sort them
+  sides = []
+  sides << a
+  sides << b
+  sides << c
+  sides.sort!
+
+  # check that none of them are zeros
+  sides.each do |side|
+    unless side > 0
+      raise TriangleError
+    end
+  end
+
+  # unless we're using an equilateral, the sum
+  # of the two shortest sides should be longer
+  # than the longest side
+  unless sides[0] == sides[2]
+    if sides[0] && sides[1] == 1
+      raise TriangleError
+    elsif (sides[0] + sides[1]) <= sides[2]
+      raise TriangleError
+    end
+  end
+
+  # if no errors are raised, classify that bad-boy
+  if sides[0] == sides[2]
+    :equilateral
+  # we can have two sides of the same length in
+  # two different configurations: [2, 2, 3] or [2, 3, 3]
+  elsif (sides[0] < sides[1] && sides[1] == sides[2]) ||
+        (sides[0] == sides[1] && sides[1] < sides[2])
+    :isosceles
+  elsif sides[0] < sides[1] && sides[1] < sides[2]
+    :scalene
+  end
 end
 
 # Error class used in part 2.  No need to change this code.
