9ba35f3e8ff81e586114004f901146ee667d94fa
Joshua Sandlin <joshua.sandlin@gmail.com>
2015-09-17 17:45:37 -0400

    amost done!

--- about_dice_project.rb
+++ about_dice_project.rb
@@ -52,8 +52,6 @@ class AboutDiceProject < Neo::Koan
     end
     second_time = dice.values
 
-    p first_time
-    p second_time
     assert_not_equal first_time, second_time,
       "Two rolls should not be equal"
 
